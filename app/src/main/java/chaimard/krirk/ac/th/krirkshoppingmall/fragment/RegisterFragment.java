62bfc4a8d5d39c520d75dc1a9c9c9deb20aa7cbc
chaimard <chaimard@email.krirk.ac.th>
2018-03-06 14:32:03 +0700

    menu

--- app/src/main/java/chaimard/krirk/ac/th/krirkshoppingmall/fragment/RegisterFragment.java
+++ app/src/main/java/chaimard/krirk/ac/th/krirkshoppingmall/fragment/RegisterFragment.java
@@ -3,10 +3,15 @@ package chaimard.krirk.ac.th.krirkshoppingmall.fragment;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.Toolbar;
 import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 
+import chaimard.krirk.ac.th.krirkshoppingmall.MainActivity;
 import chaimard.krirk.ac.th.krirkshoppingmall.R;
 
 /**
@@ -14,6 +19,74 @@ import chaimard.krirk.ac.th.krirkshoppingmall.R;
  */
 
 public class RegisterFragment extends Fragment {
+
+
+    @Override
+    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+
+//        Crete Toolbar
+
+        creteToolbar();
+
+
+    } //Main Method
+
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+
+        if (item.getItemId()==R.id.itemUpload){
+
+            //สร้างเมธอดเอง  พิมพ์ชื่อเสร็จแล้ว คลิก alt+enter สร้าง
+            uploadToserver();
+
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    private void uploadToserver() {
+
+
+    }
+
+    @Override
+    public void onCreateOptionsMenu(Menu menu, MenuInflater inflater) {
+        inflater.inflate(R.menu.menu_register,menu);
+        super.onCreateOptionsMenu(menu, inflater);
+
+    }
+
+    private void creteToolbar() {
+
+        setHasOptionsMenu(true);
+
+        Toolbar toolbar = getView().findViewById(R.id.toolbarRegister);
+        ((MainActivity)getActivity()).setSupportActionBar(toolbar);
+
+        //ใส่ข้อความบน toolbar
+        ((MainActivity) getActivity()).getSupportActionBar().setTitle(getString(R.string.register));
+        ((MainActivity) getActivity()).getSupportActionBar().setSubtitle(getString(R.string.message_have_space));
+
+        //ใส่ปุ่ม back บน toolbar
+        ((MainActivity) getActivity()).getSupportActionBar().setHomeButtonEnabled(true);
+        ((MainActivity) getActivity()).getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+        //ใส่โค้ดให้กลับไป fragment ก่อนหน้า
+        toolbar.setNavigationOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                getActivity().getSupportFragmentManager().popBackStack();
+
+            }
+        });
+
+
+
+
+    }
 
     @Nullable
     @Override
