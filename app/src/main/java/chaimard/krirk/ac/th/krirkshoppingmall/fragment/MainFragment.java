30a5da50819aa40c42e89e332c0f55ca3a12079a
chaimard <chaimard@email.krirk.ac.th>
2018-03-06 13:25:17 +0700

    replace fragment

--- app/src/main/java/chaimard/krirk/ac/th/krirkshoppingmall/fragment/MainFragment.java
+++ app/src/main/java/chaimard/krirk/ac/th/krirkshoppingmall/fragment/MainFragment.java
@@ -6,6 +6,7 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 import chaimard.krirk.ac.th.krirkshoppingmall.R;
 
@@ -15,8 +16,42 @@ import chaimard.krirk.ac.th.krirkshoppingmall.R;
 
 public class MainFragment extends Fragment {
 
+    //สร้างอันนี้อันที่ 2 หลังจากที่ไปสร้าง id จาก fragment_main.xml แล้ว
+    @Override
+    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+
+
+
+//        Register Controler    สร้างอันนี้อันที่ 3 โดยไฮไลน์ทำบรรทัด 33 แล้วกด
+        registerControler();
+
+
+    }//Main Method
+
+    private void registerControler() {
+        TextView textView = getView().findViewById(R.id.txtNewRegister);
+        textView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+//                Replace Fragment
+                getActivity().getSupportFragmentManager()
+                        .beginTransaction()
+                        .replace(R.id.contentMainFragment, new RegisterFragment())
+                        .addToBackStack(null)
+                        .commit();
+
+
+            }
+        });
+
+    }
+
     @Nullable
     @Override
+
+    //สร้างอันนี้อันแรกก่อน
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
 
         View view = inflater.inflate(R.layout.fragment_main, container, false);
