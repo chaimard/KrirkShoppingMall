f4cb67bcce9237c62657e96a4e2071f1673bbd84
chaimard <chaimard@email.krirk.ac.th>
2018-03-27 15:06:02 +0700

    get new service

--- app/src/main/java/chaimard/krirk/ac/th/krirkshoppingmall/fragment/MainFragment.java
+++ app/src/main/java/chaimard/krirk/ac/th/krirkshoppingmall/fragment/MainFragment.java
@@ -1,17 +1,26 @@
 package chaimard.krirk.ac.th.krirkshoppingmall.fragment;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import org.json.JSONArray;
+import org.json.JSONObject;
 
 import chaimard.krirk.ac.th.krirkshoppingmall.R;
+import chaimard.krirk.ac.th.krirkshoppingmall.ServiceActivity;
+import chaimard.krirk.ac.th.krirkshoppingmall.utility.GetAllDataByURL;
 import chaimard.krirk.ac.th.krirkshoppingmall.utility.MyAlert;
+import chaimard.krirk.ac.th.krirkshoppingmall.utility.MyConstant;
 
 /**
  * Created by hero on 6/3/2561.
@@ -56,9 +65,64 @@ public class MainFragment extends Fragment {
 
                 } else {
 //                    No space
+                    try {
+                        MyConstant myConstant = new MyConstant();
+                        boolean statusAboolean = true;
+                        GetAllDataByURL getAllDataByURL = new GetAllDataByURL(getActivity());
+                        getAllDataByURL.execute(myConstant.getUrlGetAllUserString());
+                        String jsonString = getAllDataByURL.get();
+                        Log.d("27MarchV1", "JSON==>" + jsonString);
+
+                        JSONArray jsonArray = new JSONArray(jsonString);
+                        String[] columnStrings = myConstant.getColumnUser();
+                        String[] loginStrings = new String[columnStrings.length];
+
+                        for (int i=0;i<jsonArray.length();i++) {
+
+                            JSONObject jsonObject = jsonArray.getJSONObject(i);
+                            if (userString.equals(jsonObject.getString(columnStrings[2]))) {
+
+                                statusAboolean = false;
+                                for (int i1=0;i1 < columnStrings.length; i1+=1) {
+                                    loginStrings[i1] = jsonObject.getString(columnStrings[i1]);
 
 
-                }
+                                }  //for i1
+                            } // if
+
+                        }//for i
+
+                        if (statusAboolean) {
+//                            User false
+                            MyAlert myAlert = new MyAlert(getActivity());
+                            myAlert.myDialog("User False",
+                                    "No This User in my DB");
+
+                        } else if (passwordString.equals(loginStrings[3])) {
+//                            Password True
+                            Toast.makeText(getActivity(),"Welcome " + loginStrings[1],
+                                    Toast.LENGTH_SHORT).show();
+
+//                            MOVE to ServiceActivity
+                            Intent intent = new Intent(getActivity(), ServiceActivity.class);
+                            intent.putExtra("Login", loginStrings);
+                            startActivity(intent);  //ให้ไปใช้ service ใหม่ กระโดดไปหน้าอื่น
+                            getActivity().finish();  //สั่งให้ล้าง service เก่า
+
+                        } else {
+//                            Password False
+                            MyAlert myAlert = new MyAlert(getActivity());
+                            myAlert.myDialog("Password False",
+                                    "Please Try again Password False");
+
+                        }
+
+                    } catch (Exception e) {
+                        e.printStackTrace();
+                    }//try
+
+
+                } //if
 
             }
         });
--- app/src/main/java/chaimard/krirk/ac/th/krirkshoppingmall/utility/MyConstant.java
+++ app/src/main/java/chaimard/krirk/ac/th/krirkshoppingmall/utility/MyConstant.java
@@ -8,6 +8,21 @@ public class MyConstant {
 
     //    About  Url
     private String urlAddUser = "http://androidthai.in.th/kir/addDataKama.php";
+    private String urlGetAllUserString = "http://androidthai.in.th/kir/getAllUserKama.php";
+
+
+    //    About Array
+    private String[] columnUser = new String[]{"id","Name", "User", "Password", "Mode"};
+
+    public String[] getColumnUser() {
+
+        return columnUser;
+    }
+
+    //ตรงนี้สร้างจากคำสั่งตัวแปร private ๅๅ  แล้วกด alt+insert เลือก getter  เมื่ออยู่ใต้ตัวแปรใด  มันจะสร้าง getter ของตัวนั้นให้
+    public String getUrlGetAllUserString() {
+        return urlGetAllUserString;
+    }
 
     public String getUrlAddUser() {
         return urlAddUser;
